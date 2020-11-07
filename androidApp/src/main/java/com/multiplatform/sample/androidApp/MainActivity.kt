16fbe839a4c4ac62d3791236f52990ebea6c9af8
dima-ko <dima@egeniq.com>
2020-11-07 19:26:02 +0200

    Moko works fine

--- androidApp/src/main/java/com/multiplatform/sample/androidApp/MainActivity.kt
+++ androidApp/src/main/java/com/multiplatform/sample/androidApp/MainActivity.kt
@@ -1,36 +1,39 @@
 package com.multiplatform.sample.androidApp
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.multiplatform.sample.androidApp.databinding.ActivityMainBinding
 import com.multiplatform.sample.androidApp.ui.ListAdapter
 import com.multiplatform.sample.shared.MainViewModel
 import dev.icerock.moko.mvvm.createViewModelFactory
 
-class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
+class MainActivity : AppCompatActivity() {
 
-    override val layoutId = R.layout.activity_main
-    override val viewModelClass: Class<MainViewModel> = MainViewModel::class.java
-    override val viewModelVariableId: Int = BR._all
-    override fun viewModelFactory(): ViewModelProvider.Factory {
-        return createViewModelFactory { MainViewModel() }
-    }
+    lateinit var viewModel: MainViewModel
 
     private var listAdapter: ListAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         binding.lifecycleOwner = this
-        viewModel.pageData.addObserver {
-            listAdapter?.countryItems = it?.items
-            listAdapter?.notifyDataSetChanged()
-        }
+        val view = binding.root
+        setContentView(view)
+
+        viewModel = ViewModelProvider(this ).get(MainViewModel::class.java)
 
         val userCountryISO = Utils.getUserCountry(this)
         val userCountry = Utils.getCountryNameByISO(userCountryISO)
         val userCountryMapped = Utils.mapCountryName(userCountry)
         listAdapter = ListAdapter(userCountryMapped)
         binding.recyclerView.adapter = listAdapter
+
+        viewModel.pageData.addObserver {
+            listAdapter?.countryItems = it?.items
+            listAdapter?.notifyDataSetChanged()
+        }
     }
 
 
