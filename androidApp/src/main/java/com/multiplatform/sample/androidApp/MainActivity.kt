d55fb85c606bb394532565c7adfa76a6156c8786
dima-ko <dima@egeniq.com>
2021-02-08 13:50:19 +0200

    Refactoring

--- androidApp/src/main/java/com/multiplatform/sample/androidApp/MainActivity.kt
+++ androidApp/src/main/java/com/multiplatform/sample/androidApp/MainActivity.kt
@@ -36,18 +36,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         binding?.lifecycleOwner = this
         val view = binding?.root
         setContentView(view)
 
-        viewModel = ViewModelProvider(this ).get(MainViewModel::class.java)
-        viewModel.setup(kermit)
-
-        val userCountryISO = Utils.getUserCountry(this)
-        val userCountry = Utils.getCountryNameByISO(userCountryISO)
-        val userCountryMapped = Utils.mapCountryName(userCountry)
-        listAdapter = ListAdapter(userCountryMapped)
+        listAdapter = ListAdapter(Utils.getUserCountryMapped(this))
         binding?.recyclerView?.adapter = listAdapter
 
         // Test for memory leaks:
@@ -57,6 +52,9 @@ class MainActivity : AppCompatActivity() {
         binding?.progressBar?.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
+
+        viewModel = ViewModelProvider(this ).get(MainViewModel::class.java)
+        viewModel.setup(kermit)
         viewModel.fetchData()
     }
 
